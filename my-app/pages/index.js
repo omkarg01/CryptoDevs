33cdba821036a17c100b407bb369420cab0ea567
omkarg01 <omkargujja01@gmail.com>
2023-02-04 12:20:46 +0530

    useEffects done

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -244,6 +244,49 @@ export default function Home() {
         return web3Provider;
     };
 
+    // useEffects are used to react to changes in state of the website
+    // The array at the end of function call represents what state changes will trigger this effect
+    // In this case, whenever the value of `walletConnected` changes - this effect will be called
+    useEffect(() => {
+        // if wallet is not connected, create a new instance of Web3Modal and connect the MetaMask wallet
+        if (!walletConnected) {
+            // Assign the Web3Modal class to the reference object by setting it's `current` value
+            // The `current` value is persisted throughout as long as this page is open
+            web3ModalRef.current = new Web3Modal({
+                network: "goerli",
+                providerOptions: {},
+                disableInjectedProvider: false,
+            });
+            connectWallet();
+
+            // Check if presale has started and ended
+            const _presaleStarted = checkIfPresaleStarted();
+            if (_presaleStarted) {
+                checkIfPresaleEnded();
+            }
+
+            getTokenIdsMinted();
+
+            // Set an interval which gets called every 5 seconds to check presale has ended
+            const presaleEndedInterval = setInterval(async function () {
+                const _presaleStarted = await checkIfPresaleStarted();
+                if (_presaleStarted) {
+                    const _presaleEnded = await checkIfPresaleEnded();
+                    if (_presaleEnded) {
+                        clearInterval(presaleEndedInterval);
+                    }
+                }
+            }, 5 * 1000);
+
+            // set an interval to get the number of token Ids minted every 5 seconds
+            setInterval(async function () {
+                await getTokenIdsMinted();
+            }, 5 * 1000);
+        }
+    }, [walletConnected]);
+
+
     
+
 
 }
